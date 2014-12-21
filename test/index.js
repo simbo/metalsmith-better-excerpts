a6ac7039bdad45744daaf5386154cf6d62f98a52
Simon Lepel <simbo@simbo.de>
2014-12-21 22:06:09 +0100

    add tests

---
+++ test/index.js
@@ -0,0 +1,118 @@
+
+var assert     = require('assert'),
+    excerpt    = require('..'),
+    Metalsmith = require('metalsmith'),
+    markdown   = require('metalsmith-markdown');
+
+var util = require('util');
+
+describe('metalsmith-better-excerpts', function() {
+
+    it('should generate excerpts with default options', function(done) {
+        Metalsmith('test/fixtures/basic')
+            .use(markdown())
+            .use(excerpt())
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad…', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should allow setting excerpts in frontmatter', function(done) {
+        Metalsmith('test/fixtures/frontmatter')
+            .use(markdown())
+            .use(excerpt())
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Tokyo rain bridge motion fetishism boat dissident long-chain hydrocarbons  disposable sprawl warehouse cardboard Kowloon neon face forwards…', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should allow to keep html tags', function(done) {
+        Metalsmith('test/fixtures/basic')
+            .use(markdown())
+            .use(excerpt({
+                stripTags: false
+            }))
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem <strong>ipsum dolor</strong> <em>sit</em> amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \nconsequat.', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should allow to override pruneLength and pruneString', function(done) {
+        Metalsmith('test/fixtures/basic')
+            .use(markdown())
+            .use(excerpt({
+                pruneLength: 50,
+                pruneString: '[...]'
+            }))
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem ipsum dolor sit amet, consectetur[...]', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should not prune if pruneLength is set to 0', function(done) {
+        Metalsmith('test/fixtures/basic')
+            .use(markdown())
+            .use(excerpt({
+                pruneLength: 0
+            }))
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \nconsequat.', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should do nothing when processing non-html files', function(done) {
+        Metalsmith('test/fixtures/basic')
+            .use(excerpt())
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal(undefined, files['index.md'].excerpt);
+                done();
+            });
+    });
+
+    it('should find an excerpt by \'more\' tag, not case-sensitive', function(done) {
+        Metalsmith('test/fixtures/more')
+            .use(markdown())
+            .use(excerpt())
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor \nincididunt ut labore et dolore magna aliqua.\nUt enim ad…', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should should allow to define a custom \'more\' tag', function(done) {
+        Metalsmith('test/fixtures/more-custom')
+            .use(markdown())
+            .use(excerpt({
+                moreRegExp: /\s*<!--\s*excerpt\s*-->/i
+            }))
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor \nincididunt ut labore et dolore magna aliqua.\nUt enim ad…', files['index.html'].excerpt);
+                done();
+            });
+    });
+
+    it('should it should set excerpt to false if all generation methods fail', function(done) {
+        Metalsmith('test/fixtures/no-excerpt')
+            .use(markdown())
+            .use(excerpt())
+            .build(function(err, files) {
+                if (err) return done(err);
+                assert.equal(false, files['index.html'].excerpt);
+                done();
+            });
+    });
+
+});
